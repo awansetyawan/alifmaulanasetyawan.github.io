273750bf142c4c30dfb4f98cfba3646269de68d7
Alif Maulana Setyawan <92102854+awansetyawan@users.noreply.github.com>
2026-06-10 07:55:06 +0800

    Develop Portfolio

---
+++ awan/js/data.js
@@ -0,0 +1,90 @@
+const data = {
+    portfolios: [
+        {
+            techStack: "Flutter (Dart), Laravel (PHP), MySQL, Firebase Cloud Messaging, & MQTT",
+            title: "SMODE",
+            githubLink: "https://github.com/awansetyawan/SMODE",
+            projectLink: "#",
+            imgLink: "images/portfolio/mobile/smode.png"
+        },
+        {
+            techStack: "Flutter (Dart), Firebase Firestore and Authentication, & REST API",
+            title: "Flutix",
+            githubLink: "https://github.com/awansetyawan/Flutix_Mobile",
+            projectLink: "#",
+            imgLink: "images/portfolio/mobile/flutix.png"
+        },
+        {
+            techStack: "HTML, CSS, JS, PHP, & MySQL",
+            title: "Galeri Gitar",
+            githubLink: "https://github.com/awansetyawan/Web_GaleriGitar",
+            projectLink: "#",
+            imgLink: "images/portfolio/website/galerigitar.png"
+        },
+        {
+            techStack: "Python & TensorFlow",
+            title: "Image Processing 5 Hewan Mamalia",
+            githubLink: "https://github.com/awansetyawan/Praktikum_KB/tree/main/Project%20Akhir",
+            projectLink: "#",
+            imgLink: "images/portfolio/ml/mamalia.png"
+        },
+    ],
+
+    certificates: [
+        {   
+            title: "Sertifikat Internship",
+            issuer: "PT. Aplikanusa Lintasarta",
+            imgLink: "images/sertifikat/la.jpg",
+        },
+        {   
+            title: "Sertifikat Internship",
+            issuer: "Dinas Pemadam Kebakaran Kota Samarinda Kalimantan Timur",
+            imgLink: "images/sertifikat/damkar.jpg",
+        },
+        {
+            title: "Sertifikat Uji Kompetensi",
+            issuer: "SMK Negeri 7 Samarinda",
+            imgLink: "images/sertifikat/uji.jpg",
+        },
+        {
+            title: "AWS Academy Cloud Foundations",
+            issuer: "AWS Academy Graduate",
+            imgLink: "images/sertifikat/aws.jpg",
+        },
+        {
+            title: "HCIA-Cloud Service V3.5",
+            issuer: "Huawei Talent Online",
+            imgLink: "images/sertifikat/hcia 3.5.jpg",
+        },
+        {
+            title: "HCIA-Cloud Computing V5.0",
+            issuer: "Huawei Talent Online",
+            imgLink: "images/sertifikat/hcia 5.0.jpg",
+        },
+        {
+            title: "IT Essentials",
+            issuer: "Cisco Networking Academy",
+            imgLink: "images/sertifikat/it-essentials.jpg",
+        },
+        {
+            title: "CCNA: Routing and Switching Essentials",
+            issuer: "Cisco Networking Academy",
+            imgLink: "images/sertifikat/ccna.jpg",
+        },
+        {
+            title: "Python For Data Science",
+            issuer: "Sololearn",
+            imgLink: "images/sertifikat/py1.png",
+        },
+        {
+            title: "C++",
+            issuer: "Sololearn",
+            imgLink: "images/sertifikat/c++.png",
+        },
+        {
+            title: "Python For Beginners",
+            issuer: "Sololearn",
+            imgLink: "images/sertifikat/py2.png",
+        },
+    ]
+}
